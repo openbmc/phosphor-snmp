f90636f96e398a9b31019cbe393a32738fe558b2
Ratan Gupta <ratagupt@in.ibm.com>
2018-04-16 15:28:36 +0530

    Implement sendTrap function
    This function gets all the objects from the specific
    notification class and send the snmp trap using the
    netsnmp lib functions.
    
    This commt adds the configure, makefile, bootstrap.sh,
    clangformat file.
    
    Add the unit test cases for ErrorNotification unit.
    
    Change-Id: I2e982f18eb2745f2c0c8de0702cc85d12e80f6e3
    Signed-off-by: Ratan Gupta <ratagupt@in.ibm.com>

--- snmp_notification.hpp
+++ snmp_notification.hpp
@@ -26,6 +26,7 @@
 #include <net-snmp/net-snmp-config.h>
 #include <net-snmp/net-snmp-includes.h>
 #include <net-snmp/agent/net-snmp-agent-includes.h>
+
 #include <sdbusplus/server.hpp>
 
 #include <experimental/any>
@@ -47,8 +48,8 @@ using OID = std::array<oid, MAX_OID_LEN>;
 using OID_LEN = size_t;
 using Type = u_char;
 
-using Value = sdbusplus::message::variant<uint32_t, uint64_t,
-                                          int32_t, std::string>;
+using Value =
+    sdbusplus::message::variant<uint32_t, uint64_t, int32_t, std::string>;
 // Generic snmp trap ID
 oid SNMPTrapOID[] = {1, 3, 6, 1, 6, 3, 1, 1, 4, 1, 0};
 
@@ -59,31 +60,26 @@ using Object = std::tuple<OID, OID_LEN, Type, Value>;
  *  @tparam T - type of object from ASN type would be decided.
  *  @returns the ASN object type.
  */
-template <typename T>
-u_char getASNType() = delete;
+template <typename T> u_char getASNType() = delete;
 
-template <>
-u_char getASNType<uint32_t>()
+template <> u_char getASNType<uint32_t>()
 {
-   return ASN_UNSIGNED;
+    return ASN_UNSIGNED;
 }
 
-template <>
-u_char getASNType<uint64_t>()
+template <> u_char getASNType<uint64_t>()
 {
-   return ASN_OPAQUE_U64;
+    return ASN_OPAQUE_U64;
 }
 
-template <>
-u_char getASNType<int32_t>()
+template <> u_char getASNType<int32_t>()
 {
-   return ASN_INTEGER;
+    return ASN_INTEGER;
 }
 
-template <>
-u_char getASNType<std::string>()
+template <> u_char getASNType<std::string>()
 {
-   return ASN_OCTET_STR;
+    return ASN_OCTET_STR;
 }
 
 /** @class Notification
@@ -97,34 +93,46 @@ u_char getASNType<std::string>()
 class Notification
 {
 
-    public:
-        Notification() = default;
-        Notification(const Notification&) = delete;
-        Notification(Notification&&) = default;
-        Notification& operator=(const Notification&) = delete;
-        Notification& operator=(Notification&&) = default;
-        virtual ~Notification() = default;
+  public:
+    Notification() = default;
+    Notification(const Notification &) = delete;
+    Notification(Notification &&) = default;
+    Notification &operator=(const Notification &) = delete;
+    Notification &operator=(Notification &&) = default;
+    virtual ~Notification() = default;
 
-        /** @brief Send the snmp trap to the configured
-         *          manager.
-         */
-        void sendTrap();
+    /** @brief Send the snmp trap to the configured
+     *          manager.
+     */
+    void sendTrap();
 
-    protected:
-        /** @brief get the SNMP notification type in the mib
-         *         defined format.
-         *         This is pure virtual function all the subclasses
-         *         need to provide its own defined type.
-         *  @returns the notification type string.
-         */
-        virtual std::pair<OID, OID_LEN> getTrapOID() = 0;
+  protected:
+    /** @brief Add the variable in the snmp pdu object.
+     *  @param[in] pdu - SNMP pdu object.
+     *  @param[in] objID -  SNMP object identifier.
+     *  @param[in] objIDLen - Object identifier length.
+     *  @param[in] type - ASN type of object.
+     *  @param[in] val - Value of the object.
+     *  @returns true on success otherwise false.
+     */
+    bool addPDUVar(netsnmp_pdu &pdu, const OID &objID, size_t objIDLen,
+                   u_char type, Value val);
 
-        /** @brief get all the objects meta data defined under
-         *         this notification.
-         */
-        virtual std::vector<Object> getFieldOIDList() = 0;
+    /** @brief get the SNMP notification type in the mib
+     *         defined format.
+     *         This is pure virtual function all the subclasses
+     *         need to provide its own defined type.
+     *  @returns the notification type string.
+     */
+    virtual std::pair<OID, OID_LEN> getTrapOID() = 0;
 
+    /** @brief get all the objects meta data defined under
+     *         this notification.
+     */
+    virtual std::vector<Object> getFieldOIDList() = 0;
 };
+
+class TestErrorNotification;
 
 /** @class ErrorNotification
  *  @brief subclass of Notification
@@ -135,79 +143,81 @@ class Notification
 class OBMCErrorNotification : public Notification
 {
 
-    private:
-        uint32_t OBMCErrorID;
-        uint64_t OBMCErrorTimestamp;
-        int32_t OBMCErrorSeverity;
-        std::string OBMCErrorMessage;
+  private:
+    uint32_t OBMCErrorID;
+    uint64_t OBMCErrorTimestamp;
+    int32_t OBMCErrorSeverity;
+    std::string OBMCErrorMessage;
 
-    public:
-        OBMCErrorNotification() = default;
-        OBMCErrorNotification(const OBMCErrorNotification&) = delete;
-        OBMCErrorNotification(OBMCErrorNotification&&) = default;
-        OBMCErrorNotification& operator=(const OBMCErrorNotification&) = delete;
-        OBMCErrorNotification& operator=(OBMCErrorNotification &&) = default;
-        ~OBMCErrorNotification() = default;
+  public:
+    OBMCErrorNotification() = default;
+    OBMCErrorNotification(const OBMCErrorNotification &) = delete;
+    OBMCErrorNotification(OBMCErrorNotification &&) = default;
+    OBMCErrorNotification &operator=(const OBMCErrorNotification &) = delete;
+    OBMCErrorNotification &operator=(OBMCErrorNotification &&) = default;
+    ~OBMCErrorNotification() = default;
 
-        /** @brief Constructor
-         *  @param[in] id - The error entry id.
-         *  @param[in] ts - The commit timestamp.
-         *  @param[in] sev - The severity of the error.
-         *  @param[in] msg - The message of the error.
-         */
-        OBMCErrorNotification(uint32_t id, uint64_t ts, int32_t sev,
-                          std::string msg) :
-            OBMCErrorID(id), OBMCErrorTimestamp(ts),
-            OBMCErrorSeverity(sev), OBMCErrorMessage(msg) {}
+    /** @brief Constructor
+     *  @param[in] id - The error entry id.
+     *  @param[in] ts - The commit timestamp.
+     *  @param[in] sev - The severity of the error.
+     *  @param[in] msg - The message of the error.
+     */
+    OBMCErrorNotification(uint32_t id, uint64_t ts, int32_t sev,
+                          std::string msg)
+        : OBMCErrorID(id), OBMCErrorTimestamp(ts), OBMCErrorSeverity(sev),
+          OBMCErrorMessage(msg)
+    {
+    }
 
-    protected:
+  protected:
+    std::pair<OID, OID_LEN> getTrapOID() override
+    {
+        // notification sub types
+        OID id = {1, 3, 6, 1, 4, 1, 49871, 1, 0, 1};
+        OID_LEN idLen = 10;
+        return std::make_pair<OID, OID_LEN>(std::move(id), std::move(idLen));
+    }
 
-        std::pair<OID, OID_LEN> getTrapOID() override
-        {
-            // notification sub types
-            OID id =  {1, 3, 6, 1, 4, 1, 49871, 1, 0, 1};
-            OID_LEN idLen = 10;
-            return std::make_pair<OID, OID_LEN>(std::move(id), std::move(idLen));
-        }
+    std::vector<Object> getFieldOIDList() override
+    {
+        std::vector<Object> objectList;
 
-        std::vector<Object> getFieldOIDList() override
-        {
-            std::vector<Object> objectList;
+        OID_LEN idLen = 10;
+        OID id = {1, 3, 6, 1, 4, 1, 49871, 1, 1, 1};
+        auto type = getASNType<decltype(OBMCErrorID)>();
+        auto tuple =
+            std::tuple<OID, OID_LEN, Type, Value>(id, idLen, type, OBMCErrorID);
 
-            OID_LEN idLen = 10;
-            OID id = {1, 3, 6, 1, 4, 1, 49871, 1, 1, 1};
-            auto  type = getASNType<decltype(OBMCErrorID)>();
-            auto tuple = std::tuple<OID, OID_LEN, Type, Value>(
-                    id, idLen, type, OBMCErrorID);
+        objectList.emplace_back(std::move(tuple));
 
-            objectList.emplace_back(std::move(tuple));
+        id = {1, 3, 6, 1, 4, 1, 49871, 1, 1, 2};
+        type = getASNType<decltype(OBMCErrorTimestamp)>();
+        tuple = std::tuple<OID, OID_LEN, Type, Value>(id, idLen, type,
+                                                      OBMCErrorTimestamp);
 
-            id = {1, 3, 6, 1, 4, 1, 49871, 1, 1, 2};
-            type = getASNType<decltype(OBMCErrorTimestamp)>();
-            tuple = std::tuple<OID, OID_LEN, Type, Value>(
-                    id, idLen, type, OBMCErrorTimestamp);
+        objectList.emplace_back(std::move(tuple));
 
-            objectList.emplace_back(std::move(tuple));
+        id = {1, 3, 6, 1, 4, 1, 49871, 1, 1, 3};
+        type = getASNType<decltype(OBMCErrorSeverity)>();
 
-            id = {1, 3, 6, 1, 4, 1, 49871, 1, 1, 3};
-            type = getASNType<decltype(OBMCErrorSeverity)>();
+        tuple = std::tuple<OID, OID_LEN, Type, Value>(id, idLen, type,
+                                                      OBMCErrorSeverity);
 
-            tuple = std::tuple<OID, OID_LEN, Type, Value>(
-                    id, idLen, type, OBMCErrorSeverity);
+        objectList.emplace_back(std::move(tuple));
 
-            objectList.emplace_back(std::move(tuple));
+        id = {1, 3, 6, 1, 4, 1, 49871, 1, 1, 4};
+        type = getASNType<decltype(OBMCErrorMessage)>();
 
-            id = {1, 3, 6, 1, 4, 1, 49871, 1, 1, 4};
-            type = getASNType<decltype(OBMCErrorMessage)>();
+        tuple = std::tuple<OID, OID_LEN, Type, Value>(id, idLen, type,
+                                                      OBMCErrorMessage);
 
-            tuple = std::tuple<OID, OID_LEN, Type, Value>(
-                    id, idLen, type, OBMCErrorMessage);
+        objectList.emplace_back(std::move(tuple));
 
-            objectList.emplace_back(std::move(tuple));
+        return objectList;
+    }
 
-            return objectList;
-        }
-
+    friend class TestErrorNotification;
 };
 
 } // namespace snmp
