7ebc9cb58e6ca50dc5782cf9adf1b1cb9bd8d062
Ratan Gupta <ratagupt@in.ibm.com>
2018-04-16 15:28:36 +0530

    Implement sendTrap function
    This function gets all the objects from the
    specific notification class and send the
    snmp trap.
    This function uses the netsnmp lib for sending the
    snmp trap.
    
    Add the unit test cases for ErrorNotification unit.
    
    Change-Id: I2e982f18eb2745f2c0c8de0702cc85d12e80f6e3
    Signed-off-by: Ratan Gupta <ratagupt@in.ibm.com>

---
+++ test/test_error_notification.cpp
@@ -0,0 +1,61 @@
+#include <gtest/gtest.h>
+#include <netinet/in.h>
+#include "snmp_notification.hpp"
+
+namespace phosphor
+{
+namespace network
+{
+namespace snmp
+{
+
+constexpr auto ERROR_NOTIF_FIELD_COUNT = 4;
+
+class TestErrorNotification : public testing::Test
+{
+    public:
+        ErrorNotification notif;
+        TestErrorNotification()
+        {
+            // Empty
+        }
+        std::vector<Object> getFieldOIDList()
+        {
+            return notif.getFieldOIDList();
+        }
+
+};
+
+TEST_F(TestErrorNotification, VerifyErrorNotificationFields)
+{
+    auto oidList = getFieldOIDList();
+
+    // verify the number of the fields in the notification.
+    EXPECT_EQ(ERROR_NOTIF_FIELD_COUNT, oidList.size());
+
+    // Verify the type of each field.
+    EXPECT_EQ(ASN_UNSIGNED, std::get<Type>(oidList[0]));
+
+    EXPECT_EQ(ASN_OPAQUE_U64, std::get<Type>(oidList[1]));
+    EXPECT_EQ(ASN_INTEGER, std::get<Type>(oidList[2]));
+    EXPECT_EQ(ASN_OCTET_STR, std::get<Type>(oidList[3]));
+}
+
+TEST_F(TestErrorNotification, GetASNType)
+{
+    auto  type = getASNType<uint32_t>();
+    EXPECT_EQ(ASN_UNSIGNED, type);
+
+    type = getASNType<uint64_t>();
+    EXPECT_EQ(ASN_OPAQUE_U64, type);
+
+    type = getASNType<int32_t>();
+    EXPECT_EQ(ASN_INTEGER, type);
+
+    type = getASNType<std::string>();
+    EXPECT_EQ(ASN_OCTET_STR, type);
+}
+
+}// namespec snmp
+}// namespce network
+}// namespace phosphor
