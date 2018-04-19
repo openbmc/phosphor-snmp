8a95175313775edee4df944f9f72fa546ea8333b
Ratan Gupta <ratagupt@in.ibm.com>
2018-04-19 16:55:32 +0530

    Read the snmp manager/trap receiver details from the D-bus service
    Get the snmp manager details from the D-bus service and send the
    trap to all the configured snmp managers.
    
    Resolves openbmc/openbmc#3058
    
    Change-Id: I9c59ae866ab194e92a85254ca646ecc299803f26
    Signed-off-by: Ratan Gupta <ratagupt@in.ibm.com>

---
+++ snmp_util.cpp
@@ -0,0 +1,90 @@
+#include "snmp_util.hpp"
+#include "xyz/openbmc_project/Common/error.hpp"
+
+#include <phosphor-logging/elog-errors.hpp>
+#include <phosphor-logging/log.hpp>
+
+namespace phosphor
+{
+
+using namespace phosphor::logging;
+using namespace sdbusplus::xyz::openbmc_project::Common::Error;
+
+ObjectValueTree getManagedObjects(sdbusplus::bus::bus& bus,
+                                  const std::string& service,
+                                  const std::string& objPath)
+{
+    ObjectValueTree interfaces;
+
+    auto method = bus.new_method_call(
+                      service.c_str(),
+                      objPath.c_str(),
+                      "org.freedesktop.DBus.ObjectManager",
+                      "GetManagedObjects");
+
+    auto reply = bus.call(method);
+
+    if (reply.is_method_error())
+    {
+        log<level::ERR>("Failed to get managed objects",
+                        entry("PATH=%s", objPath.c_str()));
+        elog<InternalFailure>();
+    }
+
+    reply.read(interfaces);
+    return interfaces;
+}
+
+namespace network
+{
+namespace snmp
+{
+
+static constexpr auto busName = "xyz.openbmc_project.Network.SNMP";
+static constexpr auto root = "/xyz/openbmc_project/network/snmp/manager";
+static constexpr auto clientIntf =
+    "xyz.openbmc_project.Network.Client";
+
+/** @brief Gets the sdbus object for this process.
+ *  @return the bus object.
+ */
+static auto& getBus()
+{
+    static auto bus = sdbusplus::bus::new_default();
+    return bus;
+}
+
+std::vector<std::string> getManagers()
+{
+    std::vector<std::string> managers;
+    auto& bus = getBus();
+    auto objTree = phosphor::getManagedObjects(bus, busName, root);
+    for (const auto& objIter : objTree)
+    {
+        try
+        {
+            auto& intfMap = objIter.second;
+            auto& snmpClientProps = intfMap.at(clientIntf);
+            auto& ipaddress =
+                snmpClientProps.at("Address").get<std::string>();
+            auto& port = snmpClientProps.at("Port").get<uint16_t>();
+            auto mgr = std::move(ipaddress);
+            if (port > 0)
+            {
+                mgr += ":";
+                mgr += std::to_string(port);
+            }
+            managers.push_back(mgr);
+        }
+        catch (const std::exception& e)
+        {
+            log<level::ERR>(e.what());
+        }
+    }
+    return managers;
+}
+
+
+} // namespace snmp
+} // namespace network
+} // namespace phosphor
