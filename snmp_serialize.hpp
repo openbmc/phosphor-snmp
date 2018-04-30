6c5ae3b321f30075b5614d7c72f8305ea471bbb6
Ratan Gupta <ratagupt@in.ibm.com>
2018-04-30 17:28:05 +0530

    Persist the snmp manager configuration
    This commit persist the manager configuration D-Bus objects
    and restores it once service starts.
    
    Change-Id: I1526b52870ee5dfea30e6527bad3fd12d1191a13
    Signed-off-by: Ratan Gupta <ratagupt@in.ibm.com>

---
+++ snmp_serialize.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <experimental/filesystem>
+#include "snmp_client.hpp"
+#include "config.h"
+
+namespace phosphor
+{
+namespace network
+{
+namespace snmp
+{
+
+namespace fs = std::experimental::filesystem;
+
+/** @brief Serialize and persist event D-Bus object
+ *  @param[in] manager - const reference to snmp client/manager object.
+ *  @return fs::path - pathname of persisted events file
+ */
+fs::path serialize(const Client& manager);
+
+/** @brief Deserialze a persisted event into a D-Bus object
+ *  @param[in] path - pathname of persisted manager/client file.
+ *  @param[in] manager - reference to snmp client/manager object
+ *                       which is the target of deserialization.
+ *  @return bool - true if the deserialization was successful, false otherwise.
+ */
+bool deserialize(const fs::path& path, Client& manager);
+
+} // namespace snmp
+} // namespace network
+} // namespace phosphor
